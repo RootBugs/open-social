61ed6b214b6f7b89d200e7916b011d4f23de2725
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-02-03 13:15:22 +0000

    docs: pub optimize query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,20 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+export class loadEncode {
+  encode = null;
+
+  init(encode) {
+    this.encode = encode;
+  }
+
+  get() {
+    return this.encode;
+  }
+}
+
+// // test: add_loop — setupTest
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
