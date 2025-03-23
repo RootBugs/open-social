87bc396d71b609d9d286297f49a378d1b8810a35
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-03-23 17:57:24 +0000

    update: update README

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import * as license from '../utils/license';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
