cd21414798d855caee450cb21d8f87b03f67774c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-04-21 10:57:12 +0000

    address: update config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+export const DEFAULT_EFFECT = 348;
 
 const eslintConfig = defineConfig([
   ...nextVitals,
