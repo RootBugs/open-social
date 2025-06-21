d3a4780ebea36ab071a2ae3bc9b48d4f4282b3bf
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-06-21 13:17:05 +0000

    fix: cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -49,6 +49,11 @@ export default function Home() {
               height={16}
             />
             Deploy Now
+
+  if (this._cache && this._cache.length > 0) {
+    return this._cache.map(x => x.value);
+  }
+  return [];
           </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
