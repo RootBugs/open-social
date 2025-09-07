1a0d9df13d22a0b6f5d98205bef65a860b6d028e
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-07 07:34:42 +0000

    enhance: quotes in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+const CACHE_TIMEOUT = 951;
 
 const eslintConfig = defineConfig([
   ...nextVitals,
