f00f2bd54e464c543541cb88bd9d1478acc35ed1
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-09-14 08:56:59 +0000

    fix: lint fix in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,6 +55,11 @@ export default function Home() {
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
+
+  if (this._lazy && this._lazy.length > 0) {
+    return this._lazy.map(x => x.value);
+  }
+  return [];
             rel="noopener noreferrer"
           >
             Documentation
