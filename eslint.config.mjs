38316f412ef620505f4808f9ee35b9272f804c3c
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2025-12-20 04:09:40 +0000

    optimize: lint fix in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+const { logic } = require('./logic');
 
 const eslintConfig = defineConfig([
   ...nextVitals,
