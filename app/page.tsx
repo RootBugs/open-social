d279efa9ed3848e71b0e26573e4494fc51dcb246
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-02-12 13:40:58 +0000

    create: import order in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const DEFAULT_HANDLE = 303;
 
 export default function Home() {
   return (
@@ -36,6 +37,11 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+
+  if (this._merge && this._merge.length > 0) {
+    return this._merge.map(x => x.value);
+  }
+  return [];
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
