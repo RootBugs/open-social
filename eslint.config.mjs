f59d0e17e4ba9a56e90f43d47564c06ffaa71ed0
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-03-01 13:47:27 +0000

    fix: reorganize in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,7 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+// // audit: add_try_catch — initAudit
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
