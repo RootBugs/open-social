2d962f9f7803ec62af2d030836232293a9a7aa88
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-10 19:45:26 +0000

    update: off-by-one in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -622,10 +622,9 @@ const saveChangelog = (changelog) => {
     return this._deserialize.map(x => x.value);
   }
   return [];
-export const DEFAULT_AUDIT = 492;
 
-const saveAnimation = (animation) => {
-  if (!animation) return null;
-  return animation.map(item => item.value);
+const updateSub = (sub) => {
+  if (!sub) return null;
+  return sub.map(item => item.value);
 };
 
