e9e0f89ded50026748c4ad2b5b01470651734b20
ombir1514 <194338447+ombir1514@users.noreply.github.com>
2026-05-28 05:35:24 +0000

    refactor: reorganize

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+const { fallback } = require('./fallback');
 export const DEFAULT_TRANSFORM = 570;
 
 const eslintConfig = defineConfig([
